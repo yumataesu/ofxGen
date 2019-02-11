936e17a071bac370bf85d591b77551742e7a715b
YumaTaesu <uma8yuma@gmail.com>
2019-02-11 14:45:48 +0900

    update gui look

--- src/ofxGenManager.cpp
+++ src/ofxGenManager.cpp
@@ -107,38 +107,43 @@ void Manager::drawFrameGui(const std::string& parent_name) {
 	ImGuiWindowFlags window_flags = 0;
 	window_flags |= ImGuiWindowFlags_NoScrollWithMouse;
 	window_flags |= ImGuiWindowFlags_NoCollapse;
-	window_flags |= ImGuiWindowFlags_NoTitleBar;
+	//window_flags |= ImGuiWindowFlags_NoTitleBar;
 
 	auto& style = ImGui::GetStyle();
-	style.FramePadding = ImVec2(2.f, 2.f);
+	style.FramePadding = ImVec2(8.f, 2.f);
+	ImVec2 push_window_padding = style.WindowPadding;
 
 	ImVec4* colors = ImGui::GetStyle().Colors;
-
-	ImVec2 pos = ImVec2(2, 2);
-	ImVec2 size = ImVec2(206, 160);
 
 	int index = 0;
 	for (const auto& frm : frames_) {
 		colors[ImGuiCol_Button] = ImVec4(0.06f, 0.06f, 0.06f, 1.00f);
 		colors[ImGuiCol_ButtonHovered] = ImVec4(0.1f, 0.1f, 0.1f, 1.00f);
 		colors[ImGuiCol_ButtonActive] = ImVec4(0.1f, 0.1f, 0.1f, 1.00f);
-		std::string window_title = "##0" + std::to_string(index) + "|" + parent_name;
+		style.WindowPadding = push_window_padding;
+		std::string window_title = "0" + std::to_string(index) + " | " + parent_name;
 		//ImGui::SetNextWindowPos(pos);
-		ImGui::SetNextWindowSize(size);
+		//ImGui::SetNextWindowSize(size);
 		ImGui::Begin(window_title.data(), 0, window_flags);
 		auto layer = this->getByName(frm->layer_name);
+
+		ImVec2 window_size = ImGui::GetWindowSize();
+		float offset = 45.0f;
+		ImVec2 slider_size = ImVec2(offset - 22.5f, (window_size.x - offset*2) * 9 / 16);
+		ImVec2 thumbnail_size = ImVec2(window_size.x - offset*2, (window_size.x - offset*2) * 9 / 16);
+
 		if (layer != nullptr) {
 			if (layer->getAlpha() > 0.f) {
-				if (ImGui::ImageButton((ImTextureID)(uintptr_t) frm->fbo.getTexture().getTextureData().textureID, ImVec2(128, 128 / 16 * 9), ImVec2(0, 0), ImVec2(1, 1), 0)) {
+				if (ImGui::ImageButton((ImTextureID)(uintptr_t) frm->fbo.getTexture().getTextureData().textureID, thumbnail_size, ImVec2(0, 0), ImVec2(1, 1), 0)) {
 					layer->bang();
 				}
 			}
 			else {
-				if(backyard_data_map_[frm->layer_name]->isAllocated()) ImGui::ImageButton((ImTextureID)(uintptr_t) backyard_data_map_[frm->layer_name]->getTextureData().textureID, ImVec2(128, 128 / 16 * 9), ImVec2(0, 0), ImVec2(1, 1), 0);
+				if(backyard_data_map_[frm->layer_name]->isAllocated()) ImGui::ImageButton((ImTextureID)(uintptr_t) backyard_data_map_[frm->layer_name]->getTextureData().textureID, thumbnail_size, ImVec2(0, 0), ImVec2(1, 1), 0);
 			}
 		}
 		else {
-			ImGui::ImageButton((ImTextureID)(uintptr_t) frm->fbo.getTexture().getTextureData().textureID, ImVec2(128, 128 / 16 * 9), ImVec2(0, 0), ImVec2(1, 1), 0);
+			ImGui::ImageButton((ImTextureID)(uintptr_t) frm->fbo.getTexture().getTextureData().textureID, thumbnail_size, ImVec2(0, 0), ImVec2(1, 1), 0);
 		}
 
 
@@ -165,44 +170,48 @@ void Manager::drawFrameGui(const std::string& parent_name) {
 		}
 
 		if (layer != nullptr) {
-			ImGui::VSliderFloat("##v", ImVec2(24, 128 / 16 * 9), &layer->getAlpha(), 0.f, 1.f, "");
+			ImGui::VSliderFloat("##v", slider_size, &layer->getAlpha(), 0.f, 1.f, "");
 		}
 		else {
 			float zero = 0.f;
-			ImGui::VSliderFloat("##v", ImVec2(24, 128 / 16 * 9), &zero, 0.f, 1.f, "");
-		}
-
-
-		ImGui::SameLine();
-		ImGui::PushItemWidth(50.5f);
-		if (ImGui::Button("", ImVec2(20, 20))) {
-			this->remove(frm->layer_name);
-			frm->clear();
+			ImGui::VSliderFloat("##v", slider_size, &zero, 0.f, 1.f, "");
 		}
 
 
 		// Layer remove UI --------------------------------------------------
 		//
 		//
+		ImGui::SameLine();
+		if (ImGui::Button("", ImVec2(slider_size.x, slider_size.x))) {
+			this->remove(frm->layer_name);
+			frm->clear();
+		}
+
 		colors[ImGuiCol_Button] = ImVec4(0.00f, 0.10f, 0.38f, 1.00f);
 		colors[ImGuiCol_ButtonHovered] = ImVec4(0.00f, 0.59f, 0.66f, 1.00f);
 		colors[ImGuiCol_ButtonActive] = ImVec4(0.00f, 1.00f, 1.00f, 1.00f);
-		ImGui::BeginChild(window_title.data(), ImVec2(184, 62), true);
-		
+		//ImGui::BeginChild(window_title.data(), ImVec2(thumbnail_size.x + slider_size.x + style.FramePadding.x * 2, 62), true);
+		//ImGui::BeginChild(window_title.data(), ImVec2(window_size.x-8.f, 62), true);
+		style.WindowPadding = ImVec2(20.f, 4.f);
+		ImGui::Spacing();
+		ImGui::Separator();
+		ImGui::Spacing();
 
+		ImGui::PushItemWidth(window_size.x * 0.7);
+		float v = 0.f;
+		ImGui::Text("Params:");
+		//ImGui::SliderFloat("tesdt", &v, 0.0f, 100.f);
+		//ImGui::SliderFloat("tesdt", &v, 0.0f, 100.f);
 		if (layer != nullptr) {
 			layer->drawGui();
 		}
-		ImGui::EndChild();
+		//ImGui::EndChild();
 		ImGui::End();
 
 		index++;
-		pos.x += size.x;
-		if(index % 2 == 0) {
-		    pos.x = 2;
-		    pos.y += size.y + 2;
-		}
 	}
+
+	style.WindowPadding = push_window_padding;
 
 	//colors[ImGuiCol_Button] = ImVec4(0.00f, 0.00f, 0.00f, 1.00f);
 	//colors[ImGuiCol_ButtonHovered] = ImVec4(0.00f, 0.00f, 0.00f, 1.00f);
@@ -343,15 +352,15 @@ void Manager::drawBackyardGui() {
 	int view_index = 1;
 	for (auto& data : backyard_data_map_) {
 		std::string window_title = "GEN-PREVIEW" + std::to_string(view_index);
-		ImGui::BeginChild(window_title.data(), ImVec2(75, 42));
-		if (data.second->isAllocated()) ImGui::ImageButton((ImTextureID)(uintptr_t)data.second->getTextureData().textureID, ImVec2(66, 37));
+		//ImGui::BeginChild(window_title.data(), ImVec2(75, 42));
+		if (data.second->isAllocated()) ImGui::ImageButton((ImTextureID)(uintptr_t)data.second->getTextureData().textureID, ImVec2(80, 45));
 		if (ImGui::BeginDragDropSource()) {
 			ImGui::SetDragDropPayload("_Gen", &data.first, sizeof(data.first), ImGuiCond_Once);
 			ImGui::ImageButton((ImTextureID)(uintptr_t)data.second->getTextureData().textureID, ImVec2(128, 72), ImVec2(0.f, 0.f), ImVec2(1.f, 1.f), 0);
 			ImGui::EndDragDropSource();
 		}
-		ImGui::EndChild();
-		if (view_index % 4 != 0) ImGui::SameLine();
+		//ImGui::EndChild();
+		if (view_index % 7 != 0) ImGui::SameLine();
 		view_index++;
 	}
 	ImGui::End();
